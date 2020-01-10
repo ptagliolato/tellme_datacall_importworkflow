fde1a7eeb7115e21de7ebda7255bce647e1f2cb2
Paolo Tagliolato <ptagliolato@users.noreply.github.com>
2020-01-10 12:11:11 +0100

    Update inspect_shape_geometry.sh

--- scripts/inspect_shape_geometry.sh
+++ scripts/inspect_shape_geometry.sh
@@ -2,4 +2,4 @@
 # outputs 3 tab separated columns: filename (with relative path), layername, ogrinfo-detected-geometryType
 
 #tree -if --noreport | grep shp$ | awk '{print "ogrinfo " $0}' | bash | awk '/INFO/{gsub(/INFO: Open of `/,"",$0); gsub(/''/,"",$0); printf("%s\t", $0)} /1:/{gsub(/1:/,"",$0);gsub(/\(/,"\t",$0);gsub(/\)/,"",$0);print $0}'
-tree -if --noreport | grep shp$ | awk '{print "FILE=" $0 "; ogrinfo -q " $0}' | bash | awk -v FILE="$(echo $FILE)" -F=": | \(|\)" '{gsub("1: ","",$1); output=sprintf("%s\t%s\t%s", FILE, $1, $2);print output}'
+for i in $(tree -if --noreport | grep shp$); do ogrinfo -q $i | awk -F="\: | \(|\)" -v fpath="$i" '{gsub("1: ","",$1); output=sprintf("%s\t%s\t%s", fpath, $1, $2);print output}'; done;
