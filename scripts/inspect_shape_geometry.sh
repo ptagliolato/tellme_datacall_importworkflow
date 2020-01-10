91fbea8aaf6c6691cbb4413873aaf376e8d31bc8
Paolo Tagliolato <ptagliolato@users.noreply.github.com>
2020-01-10 11:49:20 +0100

    Update inspect_shape_geometry.sh

--- scripts/inspect_shape_geometry.sh
+++ scripts/inspect_shape_geometry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #tree -if --noreport | grep shp$ | awk '{print "ogrinfo " $0}' | bash | awk '/INFO/{gsub(/INFO: Open of `/,"",$0); gsub(/''/,"",$0); printf("%s\t", $0)} /1:/{gsub(/1:/,"",$0);gsub(/\(/,"\t",$0);gsub(/\)/,"",$0);print $0}'
-tree -if --noreport | grep shp$ | awk '{print "FILE=" $0 "; ogrinfo -q " $0}' | bash | awk -v FILE="$(echo $FILE)" '{output=sprintf("%s\t%s\t%s", FILE, $2, $3);print output}'
+tree -if --noreport | grep shp$ | awk '{print "FILE=" $0 "; ogrinfo -q " $0}' | bash | awk -v FILE="$(echo $FILE)" -F=": | \(|\)" '{gsub("1: ","",$1); output=sprintf("%s\t%s\t%s", FILE, $1, $2);print output}'
